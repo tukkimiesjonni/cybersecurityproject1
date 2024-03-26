728c51dccb0a5b643a6c6376dd0640a9f2d159ea
Tukkimies Jonni E <jonnituk@cubbli22-gold.local>
2024-03-26 09:58:45 +0200

    Added a new route for creating threads

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, request, redirect, session
 import users
+import threads
 
 
 @app.route("/")
@@ -20,19 +21,19 @@ def register():
         password = request.form["password"]
 
         if len(username) < 1:
-            return render_template("error.html", message="Username cannot be empty")
+            return render_template("message.html", message="Username cannot be empty")
         if len(username) > 20:
-            return render_template("error.html", message="Username must be under 20 characters")
+            return render_template("message.html", message="Username must be under 20 characters")
         if len(password) < 1:
-            return render_template("error.html", message="Password cannot be empty")
+            return render_template("message.html", message="Password cannot be empty")
         if len(password) > 100:
-            return render_template("error.html", message="Password must be under 100 characters")
+            return render_template("message.html", message="Password must be under 100 characters")
         
         if users.check_if_user_exists(username):
-            return render_template("error.html", message="Username is already taken")
+            return render_template("message.html", message="Username is already taken")
         
         if not users.create_user(username, password):
-            return render_template("error.html", message="Signing up failed")
+            return render_template("message.html", message="Signing up failed")
         
         return redirect("/")
     
@@ -48,10 +49,24 @@ def login():
 
         if users.login(username, password):
             return redirect("/")
-        return render_template("error.html", message="Wrong username or password")
+        return render_template("message.html", message="Wrong username or password")
     
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+
+@app.route("/new_thread", methods=["GET", "POST"])
+def new_thread():
+    if request.method == "GET":
+        return render_template("new_thread.html")
+    
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        if threads.create_thread(title, content):
+            return render_template("message.html", message="Thread succesfully created")
+        else:
+            return render_template("message.html", message="Something went wrong")
