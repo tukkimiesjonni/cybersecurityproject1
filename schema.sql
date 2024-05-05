13da7c4a62de42d098425e561ed87e7c9d6f21e1
Tukkimies Jonni E <jonnituk@cubbli22-gold.local>
2024-05-05 16:21:10 +0300

    Commented a new schema that uses third normal form

--- schema.sql
+++ schema.sql
@@ -25,3 +25,42 @@ CREATE TABLE votes (
     thread_id INTEGER REFERENCES threads,
     vote INTEGER
 );
+
+
+/*
+
+New schema with third normal form
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    title TEXT,
+    published TIMESTAMP
+);
+
+CREATE TABLE thread_content (
+    thread_id INTEGER PRIMARY KEY REFERENCES threads,
+    content TEXT
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads,
+    comment TEXT,
+    published TIMESTAMP
+);
+
+CREATE TABLE votes (
+    id SERIAL PRIMARY KEY,
+    thread_id INTEGER REFERENCES threads,
+    vote INTEGER
+);
+
+*/
